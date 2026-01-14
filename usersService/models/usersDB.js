9a6614e23d9063328507d3b014fb5bce1124e266
Amit <amitbreg@gmail.com>
2026-01-14 21:56:05 +0200

    the code add a user display all users and display a user by id

--- usersService/models/usersDB.js
+++ usersService/models/usersDB.js
@@ -1,16 +1,22 @@
 // put here all files that work with monogoDB// 
-require('dotenv').config();
-const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 
+const mongoose = require('mongoose');
 const uri = process.env.mongoUri;
 
+// Connect to MongoDB Atlas
 mongoose.connect(uri)
   .then(() => console.log("Connected to MongoDB Atlas!"))
   .catch(err => console.error("Error connecting:", err));
 
-  const users = new mongoose.Schema({
-  id: Number ,
-  first_name: String ,
-  last_name: String ,
+// Schema definition based on requirements
+const users = new mongoose.Schema({
+  id: Number,
+  first_name: String,
+  last_name: String,
   birthday: Date
 });
+
+const User = mongoose.model('User', users);
+module.exports = User;
